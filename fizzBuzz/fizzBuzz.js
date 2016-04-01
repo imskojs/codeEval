c48dd7f0852cd7c31e53e9d2a408f47ad8edece3
Seunghoon Ko <imskojs@gmail.com>
2016-04-01 21:34:32 +0900

    No semicolon needed

--- fizzBuzz/fizzBuzz.js
+++ fizzBuzz/fizzBuzz.js
@@ -1,37 +1,41 @@
 "use strict";
 var fs = require('fs');
-var FizzBuzz = (function() {
-  function FizzBuzz() {}
-  FizzBuzz.answer = function(input) {
-    var source = fs.readFileSync(input).toString().split('\n');
-    for (var _i = 0, source_1 = source; _i < source_1.length; _i++) {
-      var line = source_1[_i];
-      if (line !== '') {
-        var letters = line.split(' ');
-        var fer = Number(letters[0]);
-        var ber = Number(letters[1]);
-        var length_1 = Number(letters[2]);
-        var numbers = [];
-        for (var i = 1; i <= length_1; i++) {
-          numbers.push(i);
-        }
-        var results = [];
-        for (var i = 0; i < numbers.length; i++) {
-          var number = numbers[i];
-          if ((number % fer) === 0 && (number % ber) === 0) {
-            results.push('FB');
-          } else if (number % fer === 0) {
-            results.push('F');
-          } else if (number % ber === 0) {
-            results.push('B');
-          } else {
-            results.push(String(number));
-          }
-        }
-        console.log(results.join(' '));
-      }
+var FizzBuzz = (function () {
+    function FizzBuzz() {
     }
-  };
-  return FizzBuzz;
+    FizzBuzz.answer = function (input) {
+        var source = fs.readFileSync(input).toString().split('\n');
+        for (var _i = 0, source_1 = source; _i < source_1.length; _i++) {
+            var line = source_1[_i];
+            if (line !== '') {
+                var letters = line.split(' ');
+                var fer = Number(letters[0]);
+                var ber = Number(letters[1]);
+                var length_1 = Number(letters[2]);
+                var numbers = [];
+                for (var i = 1; i <= length_1; i++) {
+                    numbers.push(i);
+                }
+                var results = [];
+                for (var i = 0; i < numbers.length; i++) {
+                    var number = numbers[i];
+                    if ((number % fer) === 0 && (number % ber) === 0) {
+                        results.push('FB');
+                    }
+                    else if (number % fer === 0) {
+                        results.push('F');
+                    }
+                    else if (number % ber === 0) {
+                        results.push('B');
+                    }
+                    else {
+                        results.push(String(number));
+                    }
+                }
+                console.log(results.join(' '));
+            }
+        }
+    };
+    return FizzBuzz;
 }());
 FizzBuzz.answer(process.argv[2]);
--- fizzBuzz/fizzBuzz.ts
+++ fizzBuzz/fizzBuzz.ts
@@ -1,34 +1,34 @@
-import fs = require('fs');
+import fs = require('fs')
 class FizzBuzz {
   static answer(input: string){
     let source: string[] = fs.readFileSync(input).toString().split('\n')
     for(let line of source){
       if(line !== ''){
-        let letters: string[] = line.split(' ');
-        let fer: number = Number(letters[0]);
-        let ber: number = Number(letters[1]);
-        let length: number = Number(letters[2]);
-        let numbers: number[] = [];
+        let letters: string[] = line.split(' ')
+        let fer: number = Number(letters[0])
+        let ber: number = Number(letters[1])
+        let length: number = Number(letters[2])
+        let numbers: number[] = []
         for (let i: number = 1; i <= length; i++){
-          numbers.push(i);
+          numbers.push(i)
         }
-        let results: string[] = [];
+        let results: string[] = []
         for (let i: number = 0; i < numbers.length; i++) {
-          let number = numbers[i];
+          let number = numbers[i]
           if ((number % fer) === 0 && (number % ber) === 0) {
-            results.push('FB');
+            results.push('FB')
           } else if (number % fer === 0) {
-            results.push('F');
+            results.push('F')
           } else if (number % ber === 0) {
-            results.push('B');
+            results.push('B')
           } else {
-            results.push(String(number));
+            results.push(String(number))
           }
         }
-        console.log(results.join(' '));
+        console.log(results.join(' '))
       }
     }
   }
 }
 
-FizzBuzz.answer(process.argv[2]);
+FizzBuzz.answer(process.argv[2])
