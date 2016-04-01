1583b7ace03671a78a6f3372fc441deafdd9ca84
Seunghoon Ko <imskojs@gmail.com>
2016-04-01 20:19:54 +0900

    Fizz Buzz

---
+++ fizzBuzz/fizzBuzz.ts
@@ -0,0 +1,33 @@
+import fs = require('fs');
+var answer: string;
+fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function(line: string) {
+  if (line !== '') {
+    var letters = line.split(' ');
+    var fer = Number(letters[0]);
+    var ber = Number(letters[1]);
+    var length = Number(letters[2]);
+    var numbers: number[] = [];
+    var i: number;
+    for (i = 1; i <= length; i++) {
+      numbers.push(i);
+    }
+    var results: (string | number)[] = [];
+    var numbersLength: number = numbers.length;
+    for (i = 0; i < numbersLength; i++) {
+      var number = numbers[i];
+      if ((number % fer) === 0 && (number % ber) === 0) {
+        results.push('FB');
+      } else if (number % fer === 0) {
+        results.push('F');
+      } else if (number % ber === 0) {
+        results.push('B');
+      } else {
+        results.push(number);
+      }
+    }
+    var resultText = results.join(' ');
+    resultText = resultText;
+    answer = resultText;
+  }
+  console.log(answer);
+});
