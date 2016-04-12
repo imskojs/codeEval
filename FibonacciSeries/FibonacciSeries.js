ba9886fad0312ee2cca260b09f517e5ffffb9d1f
Seunghoon Ko <imskojs@gmail.com>
2016-04-12 12:07:17 +0900

    Fibonacci Series

---
+++ FibonacciSeries/FibonacciSeries.js
@@ -0,0 +1,34 @@
+"use strict";
+var fs = require('fs');
+var FibonacciSeries = (function () {
+    function FibonacciSeries(filename) {
+        this.rawData = fs.readFileSync(filename).toString();
+        this.questions = this.rawData.split('\n').map(function (str) {
+            return Number(str);
+        });
+    }
+    FibonacciSeries.prototype.fibonacci = function (n) {
+        return dive(1, n, 0, 1);
+        function dive(n_prev, n, prev, curr) {
+            if (n_prev === n) {
+                return curr;
+            }
+            return dive(n_prev + 1, n, curr, prev + curr);
+        }
+    };
+    FibonacciSeries.prototype.setAnswer = function () {
+        var _this = this;
+        this.answers = this.questions.map(function (question) {
+            return _this.fibonacci(question);
+        });
+    };
+    FibonacciSeries.prototype.print = function () {
+        this.answers.forEach(function (answer) {
+            console.log(answer);
+        });
+    };
+    return FibonacciSeries;
+}());
+var fibonacciSeries = new FibonacciSeries(process.argv[2]);
+fibonacciSeries.setAnswer();
+fibonacciSeries.print();
---
+++ FibonacciSeries/FibonacciSeries.ts
@@ -0,0 +1,42 @@
+import fs = require('fs');
+
+class FibonacciSeries {
+  rawData: string;
+  questions: Array<number>;
+  answers: Array<number>;
+
+  constructor(filename: string) {
+    this.rawData = fs.readFileSync(filename).toString();
+    this.questions = this.rawData.split('\n').map((str: string):number => {
+      return Number(str);
+    })
+  }
+
+  fibonacci(n: number): number {
+    return dive(1, n, 0, 1);
+
+    function dive(n_prev: number, n: number, prev: number, curr: number): number {
+      if (n_prev === n){
+        return curr;
+      }
+      return dive(n_prev + 1, n, curr, prev + curr);
+    }
+  }
+
+  setAnswer(): void {
+    this.answers = this.questions.map((question: number): number =>{
+      return this.fibonacci(question);
+    })
+  }
+
+  print(): void {
+    this.answers.forEach((answer: number): void => {
+      console.log(answer);
+    })
+  }
+}
+
+let fibonacciSeries: FibonacciSeries = 
+  new FibonacciSeries(process.argv[2]);
+fibonacciSeries.setAnswer()
+fibonacciSeries.print();
